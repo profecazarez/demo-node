b579e048f9359083790214bcba3d85551bc177f0
profecazarez <manuel.cazarez@info.uas.edu.mx>
2026-05-19 16:31:39 -0700

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DB_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
