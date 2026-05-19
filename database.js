55b391fd76e09d619045d40f271b720fd4a5b42e
profecazarez <manuel.cazarez@info.uas.edu.mx>
2026-05-19 16:28:50 -0700

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,15 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './ventas_db.sqlite', // Archivo donde se guardarán los datos
-    logging: false
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Crucial para que Render acepte la conexión segura
+    }
+  },
+  logging: false // Cambia a console.log si quieres ver las consultas SQL en la terminal
 });
 
 export default sequelize;
